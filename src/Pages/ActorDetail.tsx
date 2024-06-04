9f894cfefdbf9dd351c5d9cb59e02d1aecd91259
arieldelg <arieldelgrande@hotmail.com>
2024-06-04 16:04:07 -0700

    Fixing bugs ActorDetail Page

--- src/Pages/ActorDetail.tsx
+++ src/Pages/ActorDetail.tsx
@@ -4,13 +4,14 @@ import { useEffect, useState } from "react";
 import { titles } from "../stylesTailwind/variables";
 
 type About = {
-  Height: string;
-  Hobbies: string;
+  Height?: string;
+  Hobbies?: string;
   Hometown: string;
-  Instagram: string;
-  Profile: string;
-  Skills_and_Abilities: string;
+  Instagram?: string;
+  Profile?: string;
+  Skills_and_Abilities?: string;
   Twitter: string;
+  ["Personal info"]: string;
 };
 
 const ActorDetail = () => {
@@ -18,8 +19,8 @@ const ActorDetail = () => {
   const { id } = url.state;
   const [actorData, setActorData] = useState<ActorVoice>();
   const [birthday, setBirthday] = useState<string>("");
-  const [age, setAge] = useState<number>(0);
-  const [actorAbout, setActorAbout] = useState<About>();
+  const [age, setAge] = useState<number | "No info">(0);
+  const [actorAbout, setActorAbout] = useState<About | "No info">();
   useEffect(() => {
     fetch(`https://api.jikan.moe/v4/people/${id}/full`)
       .then((res) => {
@@ -30,55 +31,81 @@ const ActorDetail = () => {
       })
       .then((data: ActorVoiceData) => {
         setActorData(data.data);
-        const datesplit = data.data.birthday.split("T");
-        const dayMonthYear = datesplit[0];
-        const day = dayMonthYear.split("-")[2];
-        const month = dayMonthYear.split("-")[1];
-        const year = dayMonthYear.split("-")[0];
-        const nowDate = new Date();
-        setBirthday(`${day}/${month}/${year}`);
-        const restYear = nowDate.getFullYear() - Number(year);
-        const actualMonth = nowDate.getMonth();
-        if (actualMonth < Number(month)) {
-          setAge(restYear - 1);
+        if (data.data.birthday !== null) {
+          const datesplit = data.data.birthday.split("T");
+          const dayMonthYear = datesplit[0];
+          const day = dayMonthYear.split("-")[2];
+          const month = dayMonthYear.split("-")[1];
+          const year = dayMonthYear.split("-")[0];
+          const nowDate = new Date();
+          setBirthday(`${day}/${month}/${year}`);
+          const restYear = nowDate.getFullYear() - Number(year);
+          const actualMonth = nowDate.getMonth();
+          if (actualMonth < Number(month)) {
+            setAge(restYear - 1);
+          } else {
+            setAge(restYear);
+          }
         } else {
-          setAge(restYear);
+          setAge("No info");
+          setBirthday("No info");
         }
-        const infoSplit = data.data.about.split("\n");
-        let object = {} as About;
-        infoSplit.forEach((element) => {
-          const newSplit = element.split(":");
-          const key = newSplit[0];
-          if (key.length !== 0 && !key.includes("Profile")) {
-            if (key.includes("&amp;")) {
-              const newKey = key.replace("&amp;", "&");
+        if (data.data.about !== null) {
+          const infoSplit = data.data.about.split("\n");
+          let object = {} as About;
+          infoSplit.forEach((element) => {
+            let newSplit = [];
+            if (!element.includes(": ") && element.length > 0) {
               object = {
                 ...object,
-                [newKey]: newSplit[1].trimStart(),
+                "Personal info": element,
               };
             } else {
-              object = {
-                ...object,
-                [key]: newSplit[1].trimStart(),
-              };
+              newSplit = element.split(": ");
+              const key = newSplit[0];
+              if (newSplit[0].length > 100) {
+                const newJoin = newSplit.join(": ");
+                object = {
+                  ...object,
+                  "Personal info": newJoin,
+                };
+              } else if (key.length !== 0 && !key.includes("Profile")) {
+                if (key.includes("&amp;")) {
+                  const newKey = key.replace("&amp;", "&");
+                  object = {
+                    ...object,
+                    [newKey]: newSplit[1].trimStart(),
+                  };
+                } else {
+                  object = {
+                    ...object,
+                    [key]: newSplit[1].trimStart(),
+                  };
+                }
+              }
             }
-          }
-        });
-        setActorAbout(object);
+          });
+          setActorAbout(object);
+        } else {
+          setActorAbout("No info");
+        }
       })
       .catch((error) => console.log(error));
   }, []);
   const handleMapObject = () => {
-    const mapObject: { title: string; value: string }[] = [];
+    const mapObject: { title: string; value: string | undefined }[] = [];
+
     let key: keyof About;
-    for (key in actorAbout) {
-      if (Object.prototype.hasOwnProperty.call(actorAbout, key)) {
-        if (actorAbout !== undefined) {
-          const valueActor = actorAbout[key];
-          const title = key;
-          mapObject.push({ title: title, value: valueActor });
-        } else {
-          console.log("actorAbout is undefined");
+    if (actorAbout !== "No info") {
+      for (key in actorAbout) {
+        if (Object.prototype.hasOwnProperty.call(actorAbout, key)) {
+          if (actorAbout !== undefined) {
+            const valueActor = actorAbout[key];
+            const title = key;
+            mapObject.push({ title: title, value: valueActor });
+          } else {
+            console.log("actorAbout is undefined");
+          }
         }
       }
     }
@@ -87,7 +114,7 @@ const ActorDetail = () => {
 
   if (actorData !== undefined && actorAbout !== undefined)
     return (
-      <section className="w-full h-full px-2">
+      <section className="w-full h-full px-4">
         <div className="w-full h-96 flex items-center justify-center">
           <img src={actorData?.images.jpg.image_url} alt={actorData?.name} />
         </div>
@@ -107,15 +134,25 @@ const ActorDetail = () => {
         </div>
         <div className="full py-4">
           <h2 className={`${titles} text-center py-2`}>Info</h2>
-          <div className="w-full grid grid-cols-3 gap-y-6 gap-x-2">
-            {handleMapObject().map((element, index) => {
-              return (
-                <div key={index} className="flex flex-col items-center">
-                  <h2 className={`${titles} text-center`}>{element.title}</h2>
-                  <p className="text-center">{element.value}</p>
-                </div>
-              );
-            })}
+          <div className="w-full grid grid-cols-2 gap-y-6 gap-x-4">
+            {actorAbout !== "No info"
+              ? handleMapObject().map((element, index) => {
+                  return (
+                    <div key={index} className="flex flex-col items-center">
+                      <h2 className={`${titles} text-center`}>
+                        {element.title}
+                      </h2>
+                      {/* {element.title === "Instagram" ||
+                  element.title === "Twitter" ? (
+                    <Link to={`https://${element.value}`}>click</Link>
+                  ) : (
+                    <p>sdlkads</p>
+                  )} */}
+                      <p className="text-center text-base">{element.value}</p>
+                    </div>
+                  );
+                })
+              : "No info"}
           </div>
         </div>
       </section>
